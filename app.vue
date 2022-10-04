52c72a0bc8393d0d29d686dbdb1460084c8cb447
Caleb Rogers <caleb@calebjay.com>
2022-10-04 21:11:14 +0800

    initial commit

---
+++ app.vue
@@ -0,0 +1,17 @@
+<script setup>
+const allergies = ref([])
+
+</script>
+<template>
+  <div>
+    <input type="checkbox" id="hazelnut" value="hazelnut" v-model="allergies" />
+    <label for="hazelnut">Hazelnut</label>
+    <input type="checkbox" id="peanut" value="peanut" v-model="allergies" />
+    <label for="peanut">peanut</label>
+    <input type="checkbox" id="hotdog" value="hotdog" v-model="allergies" />
+    <label for="hotdog">hotdog</label>
+    <div>
+      Allergies: {{allergies}}
+    </div>
+  </div>
+</template>
