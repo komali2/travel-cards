6a98117b8047732da4b318fd3d5e215e843d59f1
Caleb Rogers <caleb@calebjay.com>
2022-10-05 17:27:52 +0800

    Add vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,16 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  css: ['vuetify/lib/styles/main.sass'],
   nitro: {
     preset: 'netlify-builder'
-  }
+  },
+  build: {
+    transpile: ['vuetify'],
+  },
+  vite: {
+    define: {
+      'process.env.DEBUG': false,
+    },
+  },
+
 })
